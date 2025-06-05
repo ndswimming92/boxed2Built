74a83225406d8fdc5ef2cb32c81a4880a01b2930
Nicholas Davidson <87158539+ndswimming92@users.noreply.github.com>
2025-06-05 11:54:58 -0500

    Updated PrivacyPolicyModal.tsx

--- src/components/PrivacyPolicyModal.tsx
+++ src/components/PrivacyPolicyModal.tsx
@@ -3,7 +3,7 @@ import Modal from './Modal';
 
 const PrivacyPolicyModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => (
   <Modal isOpen={isOpen} onClose={onClose}>
-    <PrivacyPolicy />
+
   </Modal>
 );
 
