89bcb249e3c4d674741947be5a2944262936b113
Nicholas Davidson <87158539+ndswimming92@users.noreply.github.com>
2025-09-30 14:12:43 -0500

    Updated ServicesPage.tsx

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -107,12 +107,23 @@ const ServicesPage: React.FC = () => {
                 <a
                   href="tel:+19316741196"
                   onClick={handlePhoneClick}
-                  className="inline-flex items-center justify-center px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md hover:shadow-lg transition-all duration-200"
+                  className="inline-flex items-center justify-center px-8 py-4 bg-green-700 hover:bg-green-800 text-white rounded-lg font-medium transition-colors"
                 >
-                  <Phone size={18} className="mr-2" />
+                  <Phone size={20} className="mr-2" />
                   <span className="mr-2">Call</span>
-                  <span className="font-bold text-white">(931) 674-1196</span>
+                  <img 
+                    src="/images/contact/phone-number.svg" 
+                    alt="(931) 674-1196" 
+                    width="120" 
+                    height="18"
+                    className="inline-block"
+                  />
                 </a>
+              </div>
+            </div>
+          </div>
+        </section>
+
         {/* Services Section */}
         <Services />
 
@@ -189,7 +200,6 @@ const ServicesPage: React.FC = () => {
               </div>
             </div>
           </div>
-        </div>
         </section>
       </main>
       <Footer />
@@ -198,17 +208,3 @@ const ServicesPage: React.FC = () => {
 };
 
 export default ServicesPage;
-  )
-}
-  )
-}
-  )
-}
-  )
-}
-  )
-}
-  )
-}
-  )
-}
