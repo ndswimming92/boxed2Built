ef1085930d2947c49533af4e0fcae6364bbda139
Nicholas Davidson <87158539+ndswimming92@users.noreply.github.com>
2026-01-17 22:23:38 -0600

    chore: target modern browsers in build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2022',
+  },
+  esbuild: {
+    target: 'es2022',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
     include: ['recharts'],
