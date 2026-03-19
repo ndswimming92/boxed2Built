77fa0865b609bfe64ff1f3a35f7150ab7fef4e60
Nicholas Davidson <87158539+ndswimming92@users.noreply.github.com>
2026-03-18 22:36:46 -0500

    Updated index.ts

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -8,7 +8,7 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
 };
 
-const stripe = new Stripe(Deno.env.get("Stripe_Sandbox_Secret_Key")!, {
+const stripe = new Stripe(Deno.env.get("Stripe_Live_Secret_Key")!, {
   apiVersion: "2024-04-10",
 });
 
