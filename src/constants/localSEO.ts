8a4cb34c01318e42a4847e5333d7cf0dd0136a22
Nicholas Davidson <87158539+ndswimming92@users.noreply.github.com>
2026-04-29 13:12:16 -0500

    Updated localSEO.ts

--- src/constants/localSEO.ts
+++ src/constants/localSEO.ts
@@ -2,7 +2,7 @@
 
 export const BUSINESS_INFO = {
   name: "Boxed2Built",
-  phone: "+16154034538",
+  phone: "+16155511402",
   phoneFormatted: "(615) 551-1402",
   email: "boxed2builtco@gmail.com",
   website: "https://boxed2built.com",
