3f1ad0fb0dd11c60c2f3d964a0c03e19b8d6bfc2
Nicholas Davidson <87158539+ndswimming92@users.noreply.github.com>
2026-05-05 21:46:20 -0500

    Updated HoursGivenBackCounter.tsx

--- src/components/sections/HoursGivenBackCounter.tsx
+++ src/components/sections/HoursGivenBackCounter.tsx
@@ -11,7 +11,7 @@ type FlipDigitProps = {
   delay: number;
 };
 
-const STEP_MS = 250;
+const STEP_MS = 185;
 const INITIAL_DELAY = 500;
 
 const FlipDigit: React.FC<FlipDigitProps> = ({ target, shouldAnimate, delay }) => {
