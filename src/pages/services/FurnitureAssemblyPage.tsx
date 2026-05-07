1a3b72feed230107a9a35d5f8201bdb4767ae9ec
Nicholas Davidson <87158539+ndswimming92@users.noreply.github.com>
2026-05-07 15:51:46 -0500

    Updated FurnitureAssemblyPage.tsx

--- src/pages/services/FurnitureAssemblyPage.tsx
+++ src/pages/services/FurnitureAssemblyPage.tsx
@@ -17,7 +17,7 @@ import { LOCAL_SEO_CONTENT, PRIMARY_SERVICES, FAQ_CONTENT, SERVICE_AREAS } from 
 
 const FurnitureAssemblyPage: React.FC = () => {
   const { data: businessData, loading } = useBusinessDataWithFallback();
-  const phoneDisplay = formatPhoneForDisplay((businessData?.info?.phone || '+16155511402').replace(/^\+1/, ''));
+  const phoneDisplay = formatPhoneForDisplay((businessData?.info?.phone || '+16154034538').replace(/^\+1/, ''));
 
   usePageMeta({
     title: LOCAL_SEO_CONTENT.furnitureAssembly.title,
