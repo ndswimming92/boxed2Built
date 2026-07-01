1fb688cd1d2d9257f7b5a34b584963d669495a46
Nicholas Davidson <87158539+ndswimming92@users.noreply.github.com>
2026-07-01 15:45:54 -0500

    Fix CORS header for Supabase client

--- supabase/functions/analyze-gallery-image/index.ts
+++ supabase/functions/analyze-gallery-image/index.ts
@@ -4,7 +4,7 @@ const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers":
-    "Content-Type, Authorization, X-Client-Info, Apikey",
+    "Content-Type, Authorization, X-Client-Info, Apikey, x-correlation-id",
 };
 
 Deno.serve(async (req: Request) => {
